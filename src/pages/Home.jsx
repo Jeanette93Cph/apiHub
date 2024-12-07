9ee64535d07ccd396372f84f6a16bf9b19294c1a
Jeanette93Cph <jeanette.93@live.dk>
2024-12-07 23:52:07 +0100

    lidt styling
    lidt styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,23 @@
 //import picture from '../assets/images/eventpic.png';
+import styled from "styled-components";
+
+
+const StyledDiv = styled.div`
+    padding-top: 20px;
+`
+
 
 function Home() {
     return ( 
         <>
         <h1>Event recommendation</h1>
         
-        <div>
+        <StyledDiv>
             <a href="https://event.xn--jeanettessgttsche-80bc.dk/api/routes">Deployed API</a>
-        </div>
-        <div>
+        </StyledDiv>
+        <StyledDiv>
             <a href="https://github.com/KamiStolt/eventRecommendation.git">Repository</a>
-        </div>
+        </StyledDiv>
         </>
      );
 }
--- src/pages/Vision.jsx
+++ src/pages/Vision.jsx
@@ -1,7 +1,7 @@
 function Vision() {
     return (
         <>
-        <h1>Vision</h1>
+        <h2>Vision</h2>
         <p>The event recommendation API shows different events as concerts, theatre and lectures based on location. We have implemented our own data for now, but in SP3, we may retrieve the data via the Ticketmaster API.</p>
         </>
       );
