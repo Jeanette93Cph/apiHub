e51a5a52efd1f3bf19c6a8b92d0485e9f87cff23
Juvenaawh <cph-jh662@cphbusiness.dk>
2024-12-08 18:40:59 +0100

    undone login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,9 @@
-import styled from "styled-components";
-
-const StyledForm = styled.form`
-  border: 1px solid black;
-  padding: 20px;
-  border-radius: 8px;
-  max-width: 400px;
-  margin: 0 auto;
-`;
-
 function Login() {
     return ( 
         <>
         <h1>Login</h1>
-        <StyledForm>
-        <p>Please login to continue</p>
-        <label>
-          Username:
-          <input type="text" placeholder="Username" />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" placeholder="Password" />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </StyledForm>
-      </>
+        <p>Please login in to continue</p>
+        </> 
     );
 }
 
